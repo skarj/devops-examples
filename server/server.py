62ec46bfd94ec074645a852d57253334d075f010
skaarj <skaarj.sergey@gmail.com>
2018-11-18 13:13:38 +0300

    progress

--- server/server.py
+++ server/server.py
@@ -6,37 +6,26 @@ from dynamodb.DBController          import DBController
 app = Flask(__name__)
 api = Api(app)
 
-IMAGES_JSON = [
-    {
-        'name': 'Falsk-restful',
-        'upload_url': 'https://flask-restful.readthedocs.io/en/0.3.5/_static/flask-restful-small.png',
-        's3_url': 'http://bucket.s3-aws-region.amazonaws.com/cccccc',
-        'timestamp': '1542143608'
-    },
-    {
-        'name': 'Docker logo',
-        'upload_url': 'https://www.docker.com/sites/default/files/Whale%20Logo332_5.png',
-        's3_url': 'http://bucket.s3-aws-region.amazonaws.com/bbbbbb',
-        'timestamp': '1542143608'
-    },
-    {
-        'name': 'AWS logo',
-        'upload_url': 'https://upload.wikimedia.org/wikipedia/commons/1/1d/AmazonWebservices_Logo.svg',
-        's3_url': 'http://bucket.s3-aws-region.amazonaws.com/aaaaaa',
-        'timestamp': '1542143608'
-    }
-]
-
 cm = ConnectionManager(mode='local', endpoint_url='http://localhost:8000')
 controller = DBController(cm)
 
 controller.checkIfTableExists()
-#controller.addImage('name2', 'ffefef', 'fefefef')
 
-class Files(Resource):
+class Image(Resource):
     def get(self):
-        return IMAGES_JSON, 200
-      
-api.add_resource(Files, "/images")
+        return 'todo', 200
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("name")
+        parser.add_argument("url")
+        args = parser.parse_args()
+
+        # if(name == images["name"]):
+        #     return "Image with name {} already exists".format(name), 400
+
+        controller.addImage(args['name'], args['url'], 'http://bucket.s3-aws-region.amazonaws.com/aaaaaa')
+
+api.add_resource(Image, "/images")
 
 app.run(debug=True)
