25b0d2511fadf59fc38f6acf325935ee6c9723c8
skaarj <skaarj.sergey@gmail.com>
2018-11-27 00:24:18 +0300

    ecr blueprint

--- stacker/blueprints/imagefetcher.py
+++ stacker/blueprints/imagefetcher.py
@@ -2,6 +2,7 @@ from stacker.blueprints.base import Blueprint
 from troposphere import Output, Ref, Template, AccountId, Join
 from troposphere.s3 import Bucket
 from troposphere.ecr import Repository
+from troposphere.codebuild import Artifacts, Environment, Source, Project
 from awacs.aws import Allow, Policy, AWSPrincipal, Statement, Principal
 import awacs.ecr as ecr
 
@@ -17,6 +18,7 @@ class Imagefetcher(Blueprint):
         }
     }
 
+
     def create_template(self):
         t = self.template
         t.add_description("Stack for imagefetcher infrastructure")
@@ -27,8 +29,9 @@ class Imagefetcher(Blueprint):
         basename = "{}Images".format(namespace).replace("-", "")
 
         images_bucket = self.create_images_bucket(basename)
-        
-        self.enable_ecr('imagefetcher')
+
+        self.create_container_registry('imagefetcher')
+        self.create_cloudbuild_project('imagefetcher')
 
         t.add_output(Output(
             "{}Bucket".format(basename),
@@ -46,8 +49,10 @@ class Imagefetcher(Blueprint):
         return bucket
 
 
-    def enable_ecr(self, name):
-    # https://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html
+    def create_container_registry(self, name):
+        """
+        https://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html
+        """
         t = self.template
 
         t.add_resource(
@@ -76,3 +81,34 @@ class Imagefetcher(Blueprint):
                 ),
             )
         )
+
+
+    def create_cloudbuild_project(self, name):
+        t = self.template
+
+        t.add_version('2010-09-09')
+
+        artifacts = Artifacts(Type='NO_ARTIFACTS')
+
+        environment = Environment(
+            ComputeType='BUILD_GENERAL1_SMALL',
+            Image='aws/codebuild/java:openjdk-8',
+            Type='LINUX_CONTAINER',
+            EnvironmentVariables=[{'Name': 'APP_NAME', 'Value': 'demo'}],
+        )
+
+        source = Source(
+            Location='codebuild-demo-test/0123ab9a371ebf0187b0fe5614fbb72c',
+            Type='S3'
+        )
+
+        project = Project(
+            "DemoProject",
+            Artifacts=artifacts,
+            Environment=environment,
+            Name='DemoProject',
+            ServiceRole='arn:aws:iam::0123456789:role/codebuild-role',
+            Source=source,
+        )
+
+        t.add_resource(project)
